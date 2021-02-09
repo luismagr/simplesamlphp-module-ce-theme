01ec1fdd29887e89bc90396c5c78ea2ff228a971
gregharvey <greg.harvey@gmail.com>
2021-02-09 12:40:15 +0100

    Wrapping authsource list in admin in a div.

---
+++ themes/ce-theme/core/authsource_list.tpl.php
@@ -0,0 +1,20 @@
+<div id="portalcontent" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+<?php
+/**
+ * Template to show list of configured authentication sources.
+ *
+ */
+$this->data['header'] = 'Test authentication sources';
+$this->includeAtTemplateBase('includes/header.php');
+?>
+  <h1><?php echo $this->data['header']; ?></h1>
+  <ul>
+<?php
+foreach ($this->data['sources'] as $id) {
+    echo '<li><a href="?as='.htmlspecialchars(urlencode($id)).'">'.htmlspecialchars($id).'</a></li>';
+}
+?>
+  </ul>
+</div>
+<?php
+$this->includeAtTemplateBase('includes/footer.php');
