76dd577129da0127be051c41f1d48b3765a38850
gregharvey <greg.harvey@gmail.com>
2021-02-10 17:11:57 +0100

    Theming error pages.

--- themes/ce-theme/core/no_metadata.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<div id="portalcontent" class="ui-tabs-panel ui-widget-content ui-corner-bottom" style="padding: 0 20px;">';
-echo '<p>'.$this->t('{core:no_metadata:config_problem}').'</p>';
-
-echo '<ul>';
-echo '<li>'.$this->t('{core:no_metadata:suggestion_user_link}').'</li>';
-echo '<li>'.$this->t('{core:no_metadata:suggestion_developer}').'</li>';
-echo '</ul>';
-echo '</div>';
